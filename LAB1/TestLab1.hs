5a44029b21871671617b551fd77af90d18be30b2
Nahuel <np254438@fi365.ort.edu.uy>
2024-09-09 23:33:55 -0300

    Ejercicios 1_3 e f g cantPropX listarProp sustCon

--- LAB1/TestLab1.hs
+++ LAB1/TestLab1.hs
@@ -145,7 +145,7 @@ sustMultTests = [TestLabel "sustMult 1" form1_sustMult_test,
 
 
 -- Todas las pruebas 
-allTests = TestList $ dobleNegTests  ++ cantPropXTests ++ listarPropTests ++ swapConTests ++ invertirTests ++ sustConTests ++ sustSimpTests ++ sustMultTests 
+allTests = TestList $ cantBinTests ++ cambiarTests ++ valoresTests ++ dobleNegTests  ++ cantPropXTests ++ listarPropTests ++ swapConTests ++ invertirTests ++ sustConTests ++ sustSimpTests ++ sustMultTests 
 -- Formulas
 
 form1 = p 
